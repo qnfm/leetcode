8701f7e7d6bce64f8289aeb61696e37635d890b0
qnfm <uzinag@163.com>
2023-12-31 15:18:01 +0000

    96 std dp

--- 96. Unique Binary Search Trees.cpp
+++ 96. Unique Binary Search Trees.cpp
@@ -20,23 +20,59 @@
 //     }
 // };
 
-#include <unordered_map>
+// With memo
+// #include <unordered_map>
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         if(n <= 0) return 0;
+//         if(n < 3) return n;
+//         if(map.contains(n)) return map[n];
+//         int sum = 0;
+//         for(int i = 1; i <= n; i++){
+//             int left = numTrees(i - 1), right = numTrees(n - i);
+//             if(left >0 && right >0) sum+= left*right;
+//             else sum+=left + right;
+//         }
+//         map[n] = sum;
+//         return sum;
+//     }
+// private:
+//     std::unordered_map<int,int> map;
+// };
 
-class Solution {
+// Standard dp
+#include <array>
+class Solution
+{
 public:
-    int numTrees(int n) {
-        if(n <= 0) return 0;
-        if(n < 3) return n;
-        if(map.contains(n)) return map[n];
-        int sum = 0;
-        for(int i = 1; i <= n; i++){
-            int left = numTrees(i - 1), right = numTrees(n - i);
-            if(left >0 && right >0) sum+= left*right;
-            else sum+=left + right;
-        }
-        map[n] = sum;
-        return sum;
+    Solution()
+    {
+        for (auto &i : dp)
+            i = -1;
     }
+    int numTrees(int n)
+    {
+        if (n <= 0)
+            return dp[0] = 0;
+        if (n < 3)
+            return n;
+        if (dp[n] != -1)
+            return dp[n];
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int left = numTrees(i - 1), right = numTrees(n - i);
+            if (left > 0 && right > 0)
+                sum += left * right;
+            else
+                sum += left + right;
+        }
+
+        return dp[n] = sum;
+    }
+
 private:
-    std::unordered_map<int,int> map;
+    // unordered_map<int,int> map;
+    std::array<int, 20> dp;
 };
