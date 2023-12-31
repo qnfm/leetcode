f0c65997457f87130d7979d29cbfb88fa261e90f
qnfm <uzinag@163.com>
2023-12-31 15:08:55 +0000

    96 memo

--- 96. Unique Binary Search Trees.cpp
+++ 96. Unique Binary Search Trees.cpp
@@ -1,21 +1,42 @@
-class Solution
-{
+// class Solution
+// {
+// public:
+//     int numTrees(int n)
+//     {
+//         if (n <= 0)
+//             return 0;
+//         if (n < 3)
+//             return n;
+//         int sum = 0;
+//         for (int i = 1; i <= n; i++)
+//         {
+//             int left = numTrees(i - 1), right = numTrees(n - i);
+//             if (left > 0 && right > 0)
+//                 sum += left * right;
+//             else
+//                 sum += left + right;
+//         }
+//         return sum;
+//     }
+// };
+
+#include <unordered_map>
+
+class Solution {
 public:
-    int numTrees(int n)
-    {
-        if (n <= 0)
-            return 0;
-        if (n < 3)
-            return n;
+    int numTrees(int n) {
+        if(n <= 0) return 0;
+        if(n < 3) return n;
+        if(map.contains(n)) return map[n];
         int sum = 0;
-        for (int i = 1; i <= n; i++)
-        {
+        for(int i = 1; i <= n; i++){
             int left = numTrees(i - 1), right = numTrees(n - i);
-            if (left > 0 && right > 0)
-                sum += left * right;
-            else
-                sum += left + right;
+            if(left >0 && right >0) sum+= left*right;
+            else sum+=left + right;
         }
+        map[n] = sum;
         return sum;
     }
+private:
+    std::unordered_map<int,int> map;
 };
