4253d8929f0283e4a82a797db08fa704572eb069
qnfm <uzinag@163.com>
2024-01-05 11:39:48 +0000

    70 recursive

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n == 0 || n == 1 || n == 2)
+            return n;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
