abaa6adf175fd68e5a42f8a6e11610d537a84303
qnfm <uzinag@163.com>
2024-01-12 08:59:40 +0000

    1704 5ms -> 2ms

--- 1704. Determine if String Halves Are Alike.cpp
+++ 1704. Determine if String Halves Are Alike.cpp
@@ -9,12 +9,14 @@ public:
     {
         unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
         int n = s.size(), mid = n / 2, freq = 0;
-        for (int i = 0; i < mid; i++)
-            if (vowels.contains(s[i]))
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i < mid && vowels.contains(s[i]))
                 freq++;
-        for (int i = mid; i < n; i++)
-            if (vowels.contains(s[i]))
+            else if (i >= mid && vowels.contains(s[i]))
                 freq--;
+        }
         return freq == 0;
     }
 };
