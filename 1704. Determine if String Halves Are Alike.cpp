f67bf21cfe72da3ba6b3a6a0c01a177df79cdeae
qnfm <uzinag@163.com>
2024-01-12 09:13:27 +0000

    1704 transform 0ms

--- 1704. Determine if String Halves Are Alike.cpp
+++ 1704. Determine if String Halves Are Alike.cpp
@@ -1,7 +1,8 @@
 #include <string>
+#include <algorithm>
 #include <unordered_set>
 using namespace std;
-
+/*
 class Solution
 {
 public:
@@ -15,6 +16,29 @@ public:
             if (i < mid && vowels.contains(s[i]))
                 freq++;
             else if (i >= mid && vowels.contains(s[i]))
+                freq--;
+        }
+        return freq == 0;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    bool halvesAreAlike(string s)
+    {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        int n = s.size(), mid = n / 2, freq = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i < mid)
+            {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                    freq++;
+            }
+            else if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
                 freq--;
         }
         return freq == 0;
