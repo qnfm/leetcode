a6c2ce8e6e1058fa178118eb6ce7246324d182c0
qnfm <uzinag@163.com>
2024-01-15 16:58:02 +0000

    1899(set)

--- 1899. Merge Triplets to Form Target Triplet.cpp
+++ 1899. Merge Triplets to Form Target Triplet.cpp
@@ -1,22 +1,20 @@
 #include <vector>
+#include <unordered_set>
 using namespace std;
 class Solution
 {
 public:
     bool mergeTriplets(vector<vector<int>> &triplets, vector<int> &target)
     {
-        vector<int> r(3, 0);
+        unordered_set<int> set;
         for (auto &t : triplets)
         {
             if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2])
                 continue;
-            if (t[0] == target[0])
-                r[0] = -1;
-            if (t[1] == target[1])
-                r[1] = -1;
-            if (t[2] == target[2])
-                r[2] = -1;
+            for (int i = 0; i < 3; i++)
+                if (t[i] == target[i])
+                    set.insert(t[i]);
         }
-        return r == vector<int>(3, -1);
+        return set.size() == 3;
     }
 };
