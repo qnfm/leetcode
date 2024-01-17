c694f30753bc394ed7bd03557056c54e6a4b9922
qnfm <uzinag@163.com>
2024-01-17 13:36:52 +0000

    678

---
+++ 678. Valid Parenthesis String.cpp
@@ -0,0 +1,34 @@
+#include <stack>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    bool checkValidString(string s)
+    {
+        int leftmin = 0, leftmax = 0;
+        for (auto c : s)
+        {
+            if (c == '(')
+            {
+                leftmin++;
+                leftmax++;
+            }
+            else if (c == ')')
+            {
+                leftmin--;
+                leftmax--;
+            }
+            else
+            {
+                leftmin--;
+                leftmax++;
+            }
+            if (leftmax < 0)
+                return false;
+            if (leftmin < 0)
+                leftmin = 0;
+        }
+        return leftmin <= 0 && leftmax >= 0;
+    }
+};
