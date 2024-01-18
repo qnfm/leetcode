3e35634755cc6b01a60a258ed4a0b55f25b6c4ac
qnfm <uzinag@163.com>
2024-01-18 09:06:22 +0000

    70-Space Optimization

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -35,21 +35,38 @@ private:
 */
 
 // Tabulation
-#include <vector>
-using namespace std;
+// #include <vector>
+// using namespace std;
+// class Solution
+// {
+// public:
+//     int climbStairs(int n)
+//     {
+//         if (n == 1)
+//             return 1;
+//         vector<int> dp(n + 1);
+//         dp[0] = dp[1] = 1;
+//         for (int i = 2; i <= n; i++)
+//         {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+//         return dp[n];
+//     }
+// };
+
+// Space optimization
 class Solution
 {
 public:
     int climbStairs(int n)
     {
-        if (n == 1)
-            return 1;
-        vector<int> dp(n + 1);
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= n; i++)
+        int cur = 0, prev = 1, twop = 0;
+        for (int i = 0; i < n; i++)
         {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            twop = cur + prev;
+            cur = prev;
+            prev = twop;
         }
-        return dp[n];
+        return prev;
     }
 };
