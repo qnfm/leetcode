7a130b7f870ebf93b94ebd2f91b6439bbc75eedb
qnfm <uzinag@163.com>
2024-01-25 11:42:56 +0000

    198 memory optiaml dp

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -31,6 +31,7 @@ private:
 };
 */
 
+/* memo
 class Solution
 {
 public:
@@ -50,5 +51,47 @@ private:
         if (map.contains(idx))
             return map[idx];
         return map[idx] = max(n[idx] + dfs(n, idx + 2), dfs(n, idx + 1));
+    }
+};
+*/
+
+// vector dp
+/*
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        const int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        vector<int> dp(n + 1, 0);
+        dp[1] = nums[0];
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[n];
+    }
+};
+*/
+
+// memory optimal dp
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        const int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        int cur = 0, prev = nums[0], np = 0;
+        for (int i = 1; i < n; i++)
+        {
+            cur = max(prev, np + nums[i]);
+            np = prev;
+            prev = cur;
+        }
+        return cur;
     }
 };
