695fa6079dfb9acfbab71d93b334c83117fb34a2
qnfm <uzinag@163.com>
2024-01-29 17:41:24 +0000

    91

--- 91. Decode Ways.cpp
+++ 91. Decode Ways.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <unordered_map>
 using namespace std;
+// recursive
+/*
 class Solution
 {
 public:
@@ -26,5 +28,33 @@ private:
             ans += helper(s, idx + 2);
         memo[idx] = ans;
         return ans;
+    }
+};
+*/
+
+// dp
+class Solution
+{
+public:
+    int numDecodings(string s)
+    {
+        if (s[0] == '0')
+            return 0;
+        int dp0, dp1 = (*s.rbegin()) == '0' ? 0 : 1, dp2 = 1;
+        int i = s.size() - 2;
+        while (i >= 0)
+        {
+            if (s[i] == '0')
+                dp0 = 0;
+            else
+            {
+                dp0 = dp1;
+                if (s[i] == '1' || (s[i] == '2' && s[i + 1] >= '0' && s[i + 1] <= '6'))
+                    dp0 += dp2;
+            }
+            dp2 = dp1, dp1 = dp0, dp0 = 0;
+            i--;
+        }
+        return dp1;
     }
 };
