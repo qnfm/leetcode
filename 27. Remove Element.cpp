eabcd5b8b82aec8a2f03677ae6566a0a3353e363
qnfm <uzinag@163.com>
2024-02-06 19:56:22 +0000

    27

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -5,12 +5,10 @@ class Solution
 public:
     int removeElement(vector<int> &nums, int val)
     {
-        int i = 0;
-        for (const auto n : nums)
-        {
+        int idx = 0;
+        for (auto n : nums)
             if (n != val)
-                nums[i++] = n;
-        }
-        return i;
+                nums[idx++] = n;
+        return idx;
     }
 };
