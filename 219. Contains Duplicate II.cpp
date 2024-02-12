4bfeb3720c771f19e0fc0c78ce868c3cc8971815
qnfm <uzinag@163.com>
2024-02-12 22:36:35 +0000

    219

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,20 @@
+#include <unordered_set>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_set<int> set;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (set.contains(nums[i]))
+                return true;
+            set.insert(nums[i]);
+            if (set.size() > k)
+                set.erase(nums[i - k]);
+        }
+        return false;
+    }
+};
