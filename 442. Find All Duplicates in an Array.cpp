7d1d9bc64a8165d96a7f53c28f3e75096845f2a5
qnfm <uzinag@163.com>
2024-03-25 09:13:49 +0000

    442

--- 442. Find All Duplicates in an Array.cpp
+++ 442. Find All Duplicates in an Array.cpp
@@ -1,6 +1,31 @@
 #include <vector>
 using namespace std;
 
+// class Solution
+// {
+// public:
+//     vector<int> findDuplicates(vector<int> &nums)
+//     {
+//         vector<int> r;
+//         for (int i = 0; i < nums.size(); ++i)
+//         {
+//             auto pos = nums[i] - 1;
+//             while (nums[pos] != nums[i])
+//             {
+//                 swap(nums[pos], nums[i]);
+//                 pos = nums[i] - 1;
+//             }
+//         }
+//         for (int i = 0; i < nums.size(); ++i)
+//         {
+//             auto pos = nums[i] - 1;
+//             if (nums[pos] == nums[i] && i != pos)
+//                 r.push_back(nums[i]);
+//         }
+//         return r;
+//     }
+// };
+
 class Solution
 {
 public:
@@ -9,18 +34,12 @@ public:
         vector<int> r;
         for (int i = 0; i < nums.size(); ++i)
         {
-            auto pos = nums[i] - 1;
-            while (nums[pos] != nums[i])
+            auto pos = abs(nums[i]) - 1;
+            if (nums[pos] < 0)
             {
-                swap(nums[pos], nums[i]);
-                pos = nums[i] - 1;
+                r.push_back(abs(nums[i]));
             }
-        }
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            auto pos = nums[i] - 1;
-            if (nums[pos] == nums[i] && i != pos)
-                r.push_back(nums[i]);
+            nums[pos] = -nums[pos];
         }
         return r;
     }
