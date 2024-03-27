9a29155479462d49006be2015b7fbcc131f3eb57
qnfm <uzinag@163.com>
2024-03-27 09:53:13 +0000

    713

--- 713. Subarray Product Less Than K.cpp
+++ 713. Subarray Product Less Than K.cpp
@@ -1,11 +1,26 @@
 #include <vector>
 using namespace std;
+
 class Solution
 {
 public:
     int numSubarrayProductLessThanK(vector<int> &nums, int k)
     {
+        if (k <= 1)
+            return 0;
         int r = 0;
-        
+        int slow = 0, fast = 0;
+        long prod = 1;
+        while (fast < nums.size())
+        {
+            prod *= nums[fast];
+            while (prod >= k && slow <= fast)
+            {
+                prod /= nums[slow++];
+            }
+            r += fast - slow + 1;
+            fast++;
+        }
+        return r;
     }
 };
