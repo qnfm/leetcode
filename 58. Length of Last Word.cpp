dc3e5dc4122469640031072c02628071a1f8b89f
qnfm <uzinag@163.com>
2024-04-01 09:37:54 +0100

    58 algo

--- 58. Length of Last Word.cpp
+++ 58. Length of Last Word.cpp
@@ -1,17 +1,34 @@
 #include <string>
 #include <sstream>
 using namespace std;
+// class Solution
+// {
+// public:
+//     int lengthOfLastWord(string s)
+//     {
+//         istringstream ss(s);
+//         string cur;
+//         int r = 0;
+//         while (ss >> cur)
+//         {
+//         }
+//         return cur.size();
+//     }
+// };
+
 class Solution
 {
 public:
     int lengthOfLastWord(string s)
     {
-        istringstream ss(s);
-        string cur;
-        int r = 0;
-        while (ss >> cur)
+        int r = 0, tail = s.size() - 1;
+        while (tail >= 0 && s[tail] == ' ')
+            --tail;
+        while (tail >= 0 && s[tail] != ' ')
         {
+            ++r;
+            --tail;
         }
-        return cur.size();
+        return r;
     }
 };
