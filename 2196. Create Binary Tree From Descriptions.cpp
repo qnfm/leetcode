e8450c09b765ed5e266e5564dd1f17b658c8f862
Lord Cormen <Jafcr@alan.org>
2024-07-15 20:25:20 +0100

    2196 cleaner

--- 2196. Create Binary Tree From Descriptions.cpp
+++ 2196. Create Binary Tree From Descriptions.cpp
@@ -1,49 +1,51 @@
-#include <vector>
 #include <unordered_map>
-#include <unordered_set>
+#include <vector>
 using namespace std;
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-class Solution
-{
-public:
-    TreeNode *createBinaryTree(vector<vector<int>> &d)
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
     {
-        // val->Node*
-        unordered_map<int, TreeNode *> map;
-        unordered_set<int> check;
-        int rootval;
-        TreeNode *root = nullptr;
-        for (const auto &no : d)
-        {
-            check.insert(no[1]);
-            TreeNode *node = new TreeNode(no[1]);
+    }
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {
+    }
+};
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions)
+    {
+        TreeNode* root = nullptr;
+        unordered_map<int, TreeNode*> map;
+        for (const auto& no : descriptions) {
+            TreeNode* node = new TreeNode(no[1]);
             map[no[1]] = node;
         }
 
-        for (const auto &no : d)
-        {
-            if (!check.contains(no[0]))
-            {
-                if (!root)
-                {
-                    rootval = no[0];
-                    root = new TreeNode(no[0]);
-                    map[rootval] = root;
-                }
+        for (const auto& no : descriptions) {
+            if (!map.contains(no[0])) {
+                root = new TreeNode(no[0]);
+                map[no[0]] = root;
             }
-            if (no[2] == 1)
+            if (no[2]) {
                 map[no[0]]->left = map[no[1]];
-            else
+            } else {
                 map[no[0]]->right = map[no[1]];
+            }
         }
-        return map[rootval];
+        return root;
     }
 };
