6c47f6e27e70a42a902d6fd18cd70534dc09f9e6
Lord Cormen <Jafcr@alan.org>
2024-07-27 12:10:31 +0100

    2405 optimize

--- 2405. Optimal Partition of String.cpp
+++ 2405. Optimal Partition of String.cpp
@@ -1,23 +1,41 @@
 #include <string>
 using namespace std;
-class Solution
-{
+// class Solution
+// {
+// public:
+//     int partitionString(string s)
+//     {
+//         int mask = 0, i = 0;
+//         int res = 1;
+//         while (i < s.size())
+//         {
+//             int val = 1 << (s[i] - 'a');
+//             if (mask & val)
+//             {
+//                 mask = 0;
+//                 res++;
+//             }
+//             mask ^= val;
+//             ++i;
+//         }
+//         return res;
+//     }
+// };
+
+class Solution {
 public:
     int partitionString(string s)
     {
-        int mask = 0, i = 0;
-        int res = 1;
-        while (i < s.size())
-        {
-            int val = 1 << (s[i] - 'a');
-            if (mask & val)
-            {
+        int mask = 0;
+        int r = 0;
+        for (auto c : s) {
+            int idx = 1 << (c - 'a');
+            if (mask & idx) {
+                ++r;
                 mask = 0;
-                res++;
             }
-            mask ^= val;
-            ++i;
+            mask ^= idx;
         }
-        return res;
+        return mask == 0 ? r : r + 1;
     }
 };
