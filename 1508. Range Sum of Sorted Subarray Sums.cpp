0a1ab10b1242ec9bb87f65604948cf760a53d633
Lord Cormen <Jafcr@alan.org>
2024-08-23 17:45:03 +0100

    1508

---
+++ 1508. Range Sum of Sorted Subarray Sums.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right)
+    {
+        vector<int> arr;
+        for (int i = 0; i < nums.size(); i++) {
+            for ()
+        }
+    }
+};
